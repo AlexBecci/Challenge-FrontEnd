76f62b631ca13cd267d50fe51fc4a97d6d620f56
Alex <beccialex2002@gmail.com>
2023-03-29 13:11:01 -0300

    Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,31 @@
 import React from "react";
 import { BsArrowDown } from "react-icons/bs";
-import { FiMessageSquare,FiHexagon } from "react-icons/fi";
+import { FiMessageSquare, FiHexagon } from "react-icons/fi";
+import { CgSlack } from "react-icons/cg";
+import { BsFillCircleFill } from "react-icons/bs";
+import { FaAsterisk } from "react-icons/fa";
+import { AiOutlineMinusCircle } from "react-icons/ai";
 
 function Home() {
   return (
     <div>
       <div className=" my-10 pb-4"></div>
-
       <div className="relative mx-auto max-w-7xl border-t border-gray-600   pb-10 font-serif  ">
         <div className="border-b border-gray-600 mb-5">
           <div className="grid max-w-lg gap-8 mx-auto mt-12 lg:grid-cols-2 lg:max-w-none   ">
             <div className="flex flex-col mb-12 mt-10 overflow-hidden ">
+              <FaAsterisk className="text-red-500 w-10 h-10 hover:animate-spin mx-16" />
+              <div className="-rotate-45">
+                <FiHexagon className="text-yellow-300 w-24 h-24 hover:animate-spin " />
+              </div>
               <div className="flex flex-col justify-between flex-1">
                 <div className="flex-1">
+                  <AiOutlineMinusCircle size={30} className="text-red-500 ml-56" />
                   <p className="block mt-2 space-y-2 ml-64">
-                    <h3 className=" px-2 text-8xl font-serif leading-none   text-white">
+                    <h3 className=" px-4 text-8xl font-serif leading-none   text-white">
                       Brother
                     </h3>
+
                     <h3 className=" text-8xl font-serif leading-none  text-zinc-700">
                       Madrid
                     </h3>
@@ -24,6 +33,10 @@ function Home() {
                       Doers School
                     </h2>
                   </p>
+                  <BsFillCircleFill
+                    size={30}
+                    className="text-red-500 mx-24 hover:animate-pulse"
+                  />
                 </div>
               </div>
             </div>
@@ -32,7 +45,7 @@ function Home() {
               <div className="flex flex-col mb-12 overflow-hidden ">
                 <div className="flex-shrink-0"></div>
                 <div className="flex flex-col justify-between flex-1">
-                  <FiHexagon/>
+                  <CgSlack className="w-14 h-14 hover:animate-spin duration-1000 text-blue-600" />
                 </div>
               </div>
 
@@ -74,6 +87,10 @@ function Home() {
                           Hay plazas disponibles. <br />
                           Apuntate
                         </p>
+                        <BsArrowDown
+                          size={20}
+                          className="-rotate-90 text-white group-hover:text-black"
+                        />
                       </div>
                     </div>
                   </div>
@@ -86,7 +103,7 @@ function Home() {
           <div>
             <h1 className=" px-20">Ver todos los cursos</h1>
             <div>
-              <button className="mx-20 mt-20 flex flex-row items-center bg-blue-600 text-white py-2 px-5 rounded-tl-xl text-sm font-semibold rounded-bl-sm hover:scale-105 duration-300 hover:bg-yellow-500">
+              <button className="mx-20 mt-5 flex flex-row items-center bg-blue-600 text-white py-2 px-5 rounded-tl-xl text-sm font-semibold rounded-bl-sm hover:scale-105 duration-300 hover:bg-yellow-500">
                 <FiMessageSquare size={20} className="mr-1" />
                 Hola, Bro :)
               </button>
@@ -95,7 +112,7 @@ function Home() {
         </div>
         <div className="ml-96 mr-96 px-8 py-10">
           <p className="text-white ">
-            <BsArrowDown className="hover:scale-125 duration-500" size={40} />
+            <BsArrowDown className="hover:scale-125 duration-500 animate-bounce" size={40} />
           </p>
         </div>
       </div>
