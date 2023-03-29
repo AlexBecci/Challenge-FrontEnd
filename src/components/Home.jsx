b9d9bbb595a19a55a87d60045a8fd56e3e045d4e
Alex <beccialex2002@gmail.com>
2023-03-29 16:54:23 -0300

    Home changed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BsArrowDown } from "react-icons/bs";
 import { FiMessageSquare, FiHexagon } from "react-icons/fi";
+import { BsHexagon } from "react-icons/bs";
 import { CgSlack } from "react-icons/cg";
 import { BsFillCircleFill } from "react-icons/bs";
 import { FaAsterisk } from "react-icons/fa";
@@ -8,17 +9,17 @@ import { AiOutlineMinusCircle } from "react-icons/ai";
 
 function Home() {
   return (
-    <div className="bg-black">
-      <div className=" py-10 pb-10 container"></div>
-      <div className="relative mx-auto max-w-7xl border-t border-gray-600   pb-10 font-serif  ">
+    <div className="bg-black w-full">
+      <div className=" py-10  container"></div>
+      <div className=" mx-auto max-w-7xl border-t border-gray-600 mt-10  font-serif  ">
         <div className="border-b border-gray-600 mb-5">
-          <div className="grid max-w-lg gap-8 mx-auto mt-12 lg:grid-cols-2 lg:max-w-none   ">
-            <div className="flex flex-col mb-12 mt-10 overflow-hidden ">
+          <div className="grid max-w-lg gap-8 mx-auto  lg:grid-cols-2 lg:max-w-none   ">
+            <div className="flex flex-col  overflow-hidden ">
               <FaAsterisk className="text-red-500 w-10 h-10 hover:animate-spin mx-16" />
               <div className="-rotate-45">
                 <FiHexagon className="text-yellow-300 w-24 h-24 hover:animate-spin " />
               </div>
-              <div className="flex flex-col justify-between flex-1">
+              <div className="flex flex-col justify-center flex-1">
                 <div className="flex-1">
                   <AiOutlineMinusCircle
                     size={30}
@@ -32,7 +33,7 @@ function Home() {
                     <h3 className=" text-8xl font-serif leading-none  text-zinc-700">
                       Madrid
                     </h3>
-                    <h2 className=" text-center py-20 text-2xl font-serif  leading-none  text-white">
+                    <h2 className=" text-center py-2 text-2xl font-serif  leading-none  text-white">
                       Doers School
                     </h2>
                   </p>
@@ -43,30 +44,27 @@ function Home() {
                 </div>
               </div>
             </div>
-
-            <div className="lg:grid lg:grid-cols-2 lg:gap-6 text-white">
-              <div className="flex flex-col mb-12 overflow-hidden ">
-                <div className="flex-shrink-0"></div>
+              <div className="flex flex-col overflow-hidden items-center sm:ml-0 sm:pl-0  -mb-1 ">
                 <div className="flex flex-col justify-between flex-1">
-                  <CgSlack className="w-14 h-14 hover:animate-spin duration-1000 text-blue-600" />
-                </div>
-              </div>
-
-              <div className="flex flex-col mb-12 overflow-hidden items-center  ">
-                <div className="flex flex-col justify-between flex-1">
-                  <div className="flex-1">
-                    <div className="flex pt-6 space-x-1 text-sm text-gray-500">
-                      <div className="border-8 w-40 h-w-40 border-orange-700 transform rotate-45 p-8 -mt-20 absolute">
-                        <div className="-ml-6 py-1">
-                          <h3 className="transform -rotate-45 text-4xl font-serif leading-none  text-orange-700 ">
-                            Ultimos <br />
-                            Lugares
-                          </h3>
+                  <div className="flex-1 -mt-16 absolute">
+                    <div className="mt-0 text-gray-400">
+                      <div className="px-2 py-1 scale-95 text-center flex flex-row  mb-10">
+                        <div>
+                          <BsHexagon
+                            className="text-orange-500 absolute -mt-1"
+                            size={200}
+                          ></BsHexagon>
+                          <div className="">
+                            <h3 className="text-2xl font-semibold group-hover:text-black  text-orange-500 mx-10  py-16 px-2">
+                              Ultimos
+                              <br /> Lugares
+                            </h3>
+                          </div>
                         </div>
                       </div>
                     </div>
-                    <div className="mt-36 text-gray-400">
-                      <div className="border border-gray-500 p-2 border-l-4 border-l-orange-500 rounded-sm group hover:bg-white hover:border-gray-100 hover:border-l-orange-700  duration-300 scale-95 hover:scale-100 ">
+                    <div className="mt-0 text-gray-400">
+                      <div className="border border-gray-500 px-2 py-1 border-l-4 border-l-orange-500 rounded-sm group hover:bg-white hover:border-gray-100 hover:border-l-orange-700  duration-300 scale-95 hover:scale-100 ">
                         <h3 className="text-2xl font-semibold group-hover:text-black  text-white ">
                           Curso Anual de Creatividad
                         </h3>
@@ -81,8 +79,8 @@ function Home() {
                         />
                       </div>
                     </div>
-                    <div className="mt-1 text-gray-400">
-                      <div className="border border-gray-500 p-2 border-l-4 border-l-orange-500 rounded-sm group hover:bg-white hover:border-gray-100 hover:border-l-orange-700 duration-300 scale-95 hover:scale-100 ">
+                    <div className=" text-gray-400">
+                      <div className="border border-gray-500 px-2 py-1 border-l-4 border-l-orange-500 rounded-sm group hover:bg-white hover:border-gray-100 hover:border-l-orange-700 duration-300 scale-95 hover:scale-100 ">
                         <h3 className="text-2xl font-semibold group-hover:text-black  text-white ">
                           Curso Anual de Creatividad
                         </h3>
@@ -122,7 +120,6 @@ function Home() {
           </p>
         </div>
       </div>
-    </div>
   );
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,16 +3,14 @@ import { HiOutlineMail } from "react-icons/hi";
 import { AiOutlineInstagram } from "react-icons/ai";
 import { TbWorld } from "react-icons/tb";
 
-
-
 function NavBar() {
   return (
-    <header className="text-gray-400 body-font bg-black ">
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <p className="flex title-font font-medium items-center text-zinc-200 mb-4 md:mb-0 ml-3 text-4xl lg:pl-36">
+    <header className="fixed text-gray-400 body-font bg-black w-full">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center absolute">
+        <p className="flex title-font font-medium items-center text-zinc-200 mb-4 md:mb-0 ml-3 text-4xl lg:pl-0">
           B
         </p>
-        <nav className="md:ml-auto md:mr-auto lg:ml-72 flex flex-wrap items-center text-base justify-center font-normal">
+        <nav className="md:ml-auto md:mr-auto lg:mx-auto flex flex-wrap items-center text-base justify-center font-normal">
           <a
             href="#s"
             className="mr-3 hover:border-b border-b-gray-600  hover:text-white hover:font-bold"
@@ -43,27 +41,27 @@ function NavBar() {
           >
             Contacto
           </a>
+          <div className="pl-10 md:-mr-36">
+            <a
+              href="#mail"
+              className="mr-0 inline-flex items-center  border-0 py-2 px-0  rounded mt-4 md:mt-0 hover:text-white hover:scale-105 duration-200"
+            >
+              <HiOutlineMail className="" size={25} />
+            </a>
+            <a
+              href="#mail"
+              className="mx-2 inline-flex items-center  border-0 py-2 px-0  rounded mt-4 md:mt-0 hover:text-white hover:scale-105 duration-200"
+            >
+              <AiOutlineInstagram className="" size={25} />
+            </a>
+            <a
+              href="#mail"
+              className="mr-12 inline-flex items-center  border-0 py-2 px-0  rounded mt-4 md:mt-0 hover:text-white hover:scale-105 duration-200"
+            >
+              <TbWorld className="" size={25} />
+            </a>
+          </div>
         </nav>
-        <div className="md:mr-20 ">
-          <a
-            href="#mail"
-            className="mr-0 inline-flex items-center  border-0 py-2 px-0  rounded mt-4 md:mt-0 hover:text-white hover:scale-105 duration-200"
-          >
-            <HiOutlineMail className="" size={25} />
-          </a>
-          <a
-            href="#mail"
-            className="mx-2 inline-flex items-center  border-0 py-2 px-0  rounded mt-4 md:mt-0 hover:text-white hover:scale-105 duration-200"
-          >
-            <AiOutlineInstagram className="" size={25} />
-          </a>
-          <a
-            href="#mail"
-            className="mr-12 inline-flex items-center  border-0 py-2 px-0  rounded mt-4 md:mt-0 hover:text-white hover:scale-105 duration-200"
-          >
-            <TbWorld className="" size={25} />
-          </a>
-        </div>
       </div>
     </header>
   );
