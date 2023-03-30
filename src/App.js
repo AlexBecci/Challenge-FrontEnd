cc2b1248ff015da44e5db8b3dedccd879b6869d8
Alex <beccialex2002@gmail.com>
2023-03-30 13:53:05 -0300

    component CheckCourse finished 2/2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Blog from "./components/Blog";
 import CheckCourse from "./components/CheckCourse";
 import ArrowYellow from "./components/CheckCourseComponents/ArrowYellow";
 import EmojiBear from "./components/CheckCourseComponents/EmojiBear";
+import ArrowWhite from "./components/CheckCourseComponents/ArrowWhite";
+import EmojiHappy from "./components/CheckCourseComponents/EmojiHappy";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Notices from "./components/Notices";
@@ -14,7 +16,7 @@ function App() {
       <Blog />
       <CheckCourse emoji={<EmojiBear />} text="Chequea todos los cursos" arrow={<ArrowYellow />} background="bg-black" textColor={"text-yellow-300"}/>
       <Notices />
-      <CheckCourse 
+      <CheckCourse emoji={<EmojiHappy/>} text="Revisa todas las noticias" arrow={<ArrowWhite/>} background="bg-rose-600" textColor={"text-gray-300"}/>
     </div>
   );
 }
