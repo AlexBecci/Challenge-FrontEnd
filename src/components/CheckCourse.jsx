069f44292432a5033042ecb3e625898f4cac7c3c
Alex <beccialex2002@gmail.com>
2023-03-31 00:37:47 -0300

    cursor pointer added in text

--- src/components/CheckCourse.jsx
+++ src/components/CheckCourse.jsx
@@ -6,13 +6,13 @@ function CheckCourse({ emoji, arrow, background, text, textColor }) {
       <div className="container px-5 py-3 mx-auto flex items-center justify-start sm:flex-row flex-col md:mr-20 md:-ml-9">
         <p className="text-sm  sm:ml-7 sm:mt-0 mt-4">{arrow}</p>
         <p className="text-sm sm:ml-7 sm:mt-0 mt-4">{emoji}</p>
-        <p className={`text-xl ${textColor} sm:ml-7 sm:mt-0 mt-4`}>{text}</p>
+        <p className={`text-xl ${textColor} sm:ml-7 sm:mt-0 mt-4 cursor-pointer`}>{text}</p>
         <p className="text-sm  sm:ml-7 sm:mt-0 mt-4">{arrow}</p>
         <p className="text-sm sm:ml-7 sm:mt-0 mt-4">{emoji}</p>
-        <p className={`text-xl ${textColor} sm:ml-7 sm:mt-0 mt-4`}>{text}</p>
+        <p className={`text-xl ${textColor} sm:ml-7 sm:mt-0 mt-4 cursor-pointer`}>{text}</p>
         <p className="text-sm  sm:ml-7 sm:mt-0 mt-4">{arrow}</p>
         <p className="text-sm sm:ml-7 sm:mt-0 mt-4">{emoji}</p>
-        <p className={`text-xl ${textColor} sm:ml-7 sm:mt-0 mt-4`}>{text}</p>
+        <p className={`text-xl ${textColor} sm:ml-7 sm:mt-0 mt-4 cursor-pointer`}>{text}</p>
       </div>
     </div>
   );
