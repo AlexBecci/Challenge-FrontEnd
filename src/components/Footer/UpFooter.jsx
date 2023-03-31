87807f3f154ce4baea4bd1630175a09d35654636
Alex <beccialex2002@gmail.com>
2023-03-30 22:39:45 -0300

    Cursos pointer in text

--- src/components/Footer/UpFooter.jsx
+++ src/components/Footer/UpFooter.jsx
@@ -4,19 +4,19 @@ function UpFooter() {
   return (
     <div className={`bg-rose-600 -mt-2`}>
       <div className="container px-5 py-3 mx-auto flex items-center justify-center sm:flex-row flex-col md:mr-20 md:ml-9">
-        <p className="text-sm  sm:ml-7 sm:mt-0 mt-4 border-l-2 border-r-2 border-black px-3">
+        <p className="cursor-pointer text-sm  sm:ml-7 sm:mt-0 mt-4 border-l-2 border-r-2 border-black px-3">
           Cursos
         </p>
-        <p className="text-sm  sm:ml-0 sm:mt-0 mt-4   border-black px-3">
+        <p className="cursor-pointer text-sm  sm:ml-0 sm:mt-0 mt-4   border-black px-3">
           Quienés somos
         </p>
-        <p className="text-sm  sm:ml-0 sm:mt-0 mt-4 border-l-2  border-black px-3">
+        <p className="cursor-pointer text-sm  sm:ml-0 sm:mt-0 mt-4 border-l-2  border-black px-3">
           Noticias Frescas
         </p>
-        <p className="text-sm  sm:ml-0 sm:mt-0 mt-4 border-l-2  border-black px-3">
+        <p className="cursor-pointer text-sm  sm:ml-0 sm:mt-0 mt-4 border-l-2  border-black px-3">
           #Wearedoers
         </p>
-        <p className="text-sm  sm:mr-7 sm:mt-0 mt-4 border-l-2 border-r-2 border-black px-3">
+        <p className="cursor-pointer text-sm  sm:mr-7 sm:mt-0 mt-4 border-l-2 border-r-2 border-black px-3">
           Contacto
         </p>
       </div>
